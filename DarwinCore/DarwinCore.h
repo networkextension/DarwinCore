114382127e3aa4757a05ff9f977c11cdd1325b75
abigt <abigt@gmail.com>
2016-11-16 09:48:05 +0800

    Initial Commit

---
+++ DarwinCore/DarwinCore.h
@@ -0,0 +1,19 @@
+//
+//  DarwinCore.h
+//  DarwinCore
+//
+//  Created by 孔祥波 on 16/11/2016.
+//  Copyright © 2016 Kong XiangBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DarwinCore.
+FOUNDATION_EXPORT double DarwinCoreVersionNumber;
+
+//! Project version string for DarwinCore.
+FOUNDATION_EXPORT const unsigned char DarwinCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DarwinCore/PublicHeader.h>
+
+
