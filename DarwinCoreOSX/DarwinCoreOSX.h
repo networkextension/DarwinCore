5406c39f5f9a76e82be925e7222e326e1ee558d4
abigt <abigt@gmail.com>
2016-11-19 19:10:16 +0800

    commit miss file

---
+++ DarwinCoreOSX/DarwinCoreOSX.h
@@ -0,0 +1,23 @@
+//
+//  DarwinCoreOSX.h
+//  DarwinCoreOSX
+//
+//  Created by 孔祥波 on 19/11/2016.
+//  Copyright © 2016 Kong XiangBo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DarwinCoreOSX.
+FOUNDATION_EXPORT double DarwinCoreOSXVersionNumber;
+
+//! Project version string for DarwinCoreOSX.
+FOUNDATION_EXPORT const unsigned char DarwinCoreOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DarwinCoreOSX/PublicHeader.h>
+
+#import <DarwinCore/DNS.h>
+//
+//#import <DarwinCore/Route.h>
+#import <DarwinCore/STTraffic.h>
+#import <DarwinCore/DNSResolver.h>
