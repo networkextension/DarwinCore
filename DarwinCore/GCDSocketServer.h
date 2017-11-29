c0ca3fa07a40ff0f7e6303f0abb2e2a448d7efd6
networkextension <b@gmail.com>
2017-11-29 18:07:14 +0800

    change type _Nonnull

--- DarwinCore/GCDSocketServer.h
+++ DarwinCore/GCDSocketServer.h
@@ -8,23 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
-typedef void (^didAcceptSocket)(int fd,NSString* ipaddr,int port);
+typedef void (^didAcceptSocket)(int fd, NSString  * _Nonnull ipaddr,int port);
 typedef void (^didClosedSocket)(int fd);
-typedef void (^incomingData)(int fd,NSData *  data);
+typedef void (^incomingData)(int fd,NSData * _Nonnull  data);
 
 @interface GCDSocketServer : NSObject
 
-@property (nonatomic) dispatch_queue_t  dispatchQueue;
+@property (nonatomic) dispatch_queue_t  _Nonnull  dispatchQueue;
 @property (nonatomic) int sfd;
-@property (nonatomic, copy) didAcceptSocket accept;
-@property (nonatomic, copy) didClosedSocket colse;
-@property (nonatomic, copy) incomingData incoming;
-@property (nonatomic) dispatch_source_t as;
-+(GCDSocketServer*)shared;
+@property (nonatomic, copy) didAcceptSocket _Nonnull accept;
+@property (nonatomic, copy) didClosedSocket _Nonnull colse;
+@property (nonatomic, copy) incomingData _Nonnull incoming;
+@property (nonatomic) dispatch_source_t _Nonnull as;
++(GCDSocketServer*_Nonnull)shared;
 
 
--(void)startServer:(int)port queue:(dispatch_queue_t  )queue;
--(void)server_write_request:(int)fd buffer:(const void *)buffer total:(size_t)total;
+-(void)startServer:(int)port queue:(dispatch_queue_t  _Nonnull)queue;
+-(void)server_write_request:(int)fd buffer:(const void *_Nonnull)buffer total:(size_t)total;
 -(void)stopServer;
 -(BOOL)running;
 @end
